3527471c9ac066a9c4d61cd3a5433eb793fb2b2f
FerdousMunmun <ferdousmunmun75@gmail.com>
2025-10-07 23:22:13 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+1.let var const
+2.Name of Variable
+3.Assingment operater
+4.Data type
+
+
+
+
+let x ="hello world"
+
+console.log(x)
+
+console.log("hello world");
+
+
+var x = 10
+var x = 5
+
+console.log(x+y);
+ var x =10
+ var x = 5
+
+ console.log(x);
+
+ let x =10
+ let x = 5
+
+ console.log(x);
+
+ let x =10
+  x = 15
+
+ console.log(x);
+
+ const pi = 3.1416
+ let pi = 4.1542
+
+ console.log(pi)
+
+scope 
+{
+    var x = "hello developer"
+}
+
+console.log(x)
+
+{
+    let x = "hello developer"
+    console.log(x);
+}
+
+jogg kora 
+
+let x = 10
+let y = 20
+let z = x+y
+console.log(z)
+
+let x = 100
+let y = 20
+let z = x-y
+console.log(z)
+
+let x = 10
+let y = 20
+let z = x*y
+console.log(z)
+
+let x = 100
+let y = 20
+let z = x/y
+console.log(z)
+
+let age = 70
+
+
+console.log("Amer age"+ " " + age)
+
+let name = "Nayeem"
+
+let age = 70;
+
+console.log("My name is" + " " + name + " " + "my bois is" + " " + age)
+
+
+let name = "FM Radio"
+
+let age = 100;
+
+console.log("My name is" + " " + name + " " + "my bois is" + " " + age)
+
+
+
+let namtaNumber = 5
+
+console.log(namtaNumber + " " + "x" + " " + 1 + " " + "=" + " " + namtaNumber*1)
+
+console.log(namtaNumber + " " + "x" + " " + 2 + " " + "=" + " " + namtaNumber*2)
+
+console.log(namtaNumber + " " + "x" + " " + 3 + " " + "=" + " " + namtaNumber*3)
+
+let age = 12
+
+if(age > 20){
+    console.log("Condition apply");
+}
+
+else{
+    console.log("condition not working");
+}
